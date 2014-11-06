06c5e4ed0e56c8a6855059e2dc0f98ef455c9bb2
udaypec <uday.pec@gmail.com>
2014-11-06 13:02:54 +0530

    Project Created
    Created the project through Eclipse

---
+++ src/org/dev/javalearnings/JavaLearningsApp.java
@@ -0,0 +1,16 @@
+package org.dev.javalearnings;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class JavaLearningsApp extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
